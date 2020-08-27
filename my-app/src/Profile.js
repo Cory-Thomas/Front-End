ad32848c21c5174afc3a2c60390ddb65a9a27330
Mary Baker <meb601@gmail.com>
2020-08-27 16:22:14 -0400

    Add 3 new articles

--- my-app/src/Profile.js
+++ my-app/src/Profile.js
@@ -43,22 +43,22 @@ export default function Profile(props) {
                     date: 'Mar 1, 2020'
                },
                 {
-                    title: 'My Encounter with a Penguin in Africa',
-                    image: 'https://images.unsplash.com/photo-1562651139-65ae77130306?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+                    title: 'Pro Tips for Hiking the Rockies',
+                    image: 'https://images.unsplash.com/photo-1568556486596-9e8ad965c80b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
                     author: 'By You',
-                    date: 'Jan 1, 2020'
+                    date: 'Apr 1, 2020'
                 },
                 {
-                    title: 'Bali: The Good, The Bad, The Ugly',
-                    image: 'https://images.unsplash.com/photo-1588665555327-a67c73b3cc23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=950&q=80',
+                    title: 'Best Croatian Beaches Revealed',
+                    image: 'https://images.unsplash.com/photo-1562685109-0a29a41dd123?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2300&q=80',
                     author: 'By You',
-                    date: 'Feb 1, 2020'
+                    date: 'May 1, 2020'
                 },
                 {
-                    title: 'Sunshine and Sailing in the South of France',
-                    image: 'https://images.unsplash.com/photo-1505080857763-eec772cd197d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80',
+                    title: 'Scoot Through Italy in Style',
+                    image: 'https://images.unsplash.com/photo-1538895490524-0ded232a96d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
                     author: 'By You',
-                    date: 'Mar 1, 2020'
+                    date: 'Jun 1, 2020'
                 }
             ];
            setUsersJournalEntries(usersJournalEntries);
